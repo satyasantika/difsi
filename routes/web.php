fff0e0efb9e48d3407882ff8635c9e27351dc70f
Satya Santika <55965465+satyasantika@users.noreply.github.com>
2023-10-15 20:16:18 +0700

    set welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome2');
 });
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth'])->name('dashboard');
